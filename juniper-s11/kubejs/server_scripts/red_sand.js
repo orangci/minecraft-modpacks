e0d8fcfa35c377a76091d189e72df0d2b5732098
Stef-00012 <me@stefdp.com>
2026-07-03 18:26:43 +0200

    forgot to save when changing percentage

--- juniper-s11/kubejs/server_scripts/red_sand.js
+++ juniper-s11/kubejs/server_scripts/red_sand.js
@@ -5,7 +5,7 @@ ServerEvents.recipes(event => {
             CreateItem.of("create:crushed_raw_lead", 0.05),
             CreateItem.of("create:crushed_raw_silver", 0.05),
             CreateItem.of("create:crushed_raw_nickel", 0.05),
-            CreateItem.of("create:crushed_raw_uranium", 0.05),
+            CreateItem.of("create:crushed_raw_uranium", 0.01),
             CreateItem.of("minecraft:stick", 0.2),
             CreateItem.of("minecraft:dead_bush", 0.15),
             CreateItem.of("minecraft:gold_nugget", 0.1),
