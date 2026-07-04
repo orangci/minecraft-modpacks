e4080625257cdc17c6b249d7d689f319432bf3f2
Stef-00012 <me@stefdp.com>
2026-07-04 12:04:56 +0200

    change waystone crafting cuz oc

--- juniper-s11/kubejs/server_scripts/waystones.js
+++ juniper-s11/kubejs/server_scripts/waystones.js
@@ -1,40 +1,40 @@
 ServerEvents.recipes(event => {
-    event.recipes.create.mechanical_crafting(
-        "waystones:warp_stone",
-        [
-            ' PPP ',
-            'PSISP',
-            'PIEIP',
-            'PSISP',
-            ' PPP '
-        ],
-        {
-            P: "endermanoverhaul:corrupted_pearl",
-            I: "cataclysm:ignitium_ingot",
-            S: "minecraft:echo_shard",
-            E: "minecraft:dragon_egg"
-        }
-    )
+    // event.recipes.create.mechanical_crafting(
+    //     "waystones:warp_stone",
+    //     [
+    //         ' PPP ',
+    //         'PSISP',
+    //         'PIEIP',
+    //         'PSISP',
+    //         ' PPP '
+    //     ],
+    //     {
+    //         P: "endermanoverhaul:corrupted_pearl",
+    //         I: "cataclysm:ignitium_ingot",
+    //         S: "minecraft:echo_shard",
+    //         E: "minecraft:dragon_egg"
+    //     }
+    // )
+
+    // event.shaped(
+    //     Item.of("waystones:waystone", 1),
+    //     [
+    //         'SLS',
+    //         'SWS',
+    //         'NDN'
+    //     ],
+    //     {
+    //         S: "minecraft:stone",
+    //         L: "minecraft:lodestone",
+    //         W: "waystones:warp_stone",
+    //         N: "minecraft:netherite_ingot",
+    //         D: "nameless_trinkets:ultimate_dust"
+    //     }
+    // )
 
     event.shaped(
         Item.of("waystones:waystone", 1),
         [
-            'SLS',
-            'SWS',
-            'NDN'
-        ],
-        {
-            S: "minecraft:stone",
-            L: "minecraft:lodestone",
-            W: "waystones:warp_stone",
-            N: "minecraft:netherite_ingot",
-            D: "nameless_trinkets:ultimate_dust"
-        }
-    )
-
-    event.shaped(
-        Item.of("waystones:waystone", 1),
-        [
             ' S ',
             'SWS',
             ' S ',
@@ -45,21 +45,21 @@ ServerEvents.recipes(event => {
         }
     )
 
-    event.shaped(
-        Item.of("waystones:mossy_waystone", 1),
-        [
-            'SLS',
-            'SWS',
-            'NDN'
-        ],
-        {
-            S: "minecraft:mossy_stone_bricks",
-            L: "minecraft:lodestone",
-            W: "waystones:warp_stone",
-            N: "minecraft:netherite_ingot",
-            D: "nameless_trinkets:ultimate_dust"
-        }
-    )
+    // event.shaped(
+    //     Item.of("waystones:mossy_waystone", 1),
+    //     [
+    //         'SLS',
+    //         'SWS',
+    //         'NDN'
+    //     ],
+    //     {
+    //         S: "minecraft:mossy_stone_bricks",
+    //         L: "minecraft:lodestone",
+    //         W: "waystones:warp_stone",
+    //         N: "minecraft:netherite_ingot",
+    //         D: "nameless_trinkets:ultimate_dust"
+    //     }
+    // )
 
     event.shaped(
         Item.of("waystones:mossy_waystone", 1),
@@ -74,21 +74,21 @@ ServerEvents.recipes(event => {
         }
     )
 
-    event.shaped(
-        Item.of("waystones:sandy_waystone", 1),
-        [
-            'SLS',
-            'SWS',
-            'NDN'
-        ],
-        {
-            S: "minecraft:chiseled_sandstone",
-            L: "minecraft:lodestone",
-            W: "waystones:warp_stone",
-            N: "minecraft:netherite_ingot",
-            D: "nameless_trinkets:ultimate_dust"
-        }
-    )
+    // event.shaped(
+    //     Item.of("waystones:sandy_waystone", 1),
+    //     [
+    //         'SLS',
+    //         'SWS',
+    //         'NDN'
+    //     ],
+    //     {
+    //         S: "minecraft:chiseled_sandstone",
+    //         L: "minecraft:lodestone",
+    //         W: "waystones:warp_stone",
+    //         N: "minecraft:netherite_ingot",
+    //         D: "nameless_trinkets:ultimate_dust"
+    //     }
+    // )
 
     event.shaped(
         Item.of("waystones:sandy_waystone", 1),
@@ -103,21 +103,21 @@ ServerEvents.recipes(event => {
         }
     )
 
-    event.shaped(
-        Item.of("waystones:deepslate_waystone", 1),
-        [
-            'SLS',
-            'SWS',
-            'NDN'
-        ],
-        {
-            S: "minecraft:deepslate",
-            L: "minecraft:lodestone",
-            W: "waystones:warp_stone",
-            N: "minecraft:netherite_ingot",
-            D: "nameless_trinkets:ultimate_dust"
-        }
-    )
+    // event.shaped(
+    //     Item.of("waystones:deepslate_waystone", 1),
+    //     [
+    //         'SLS',
+    //         'SWS',
+    //         'NDN'
+    //     ],
+    //     {
+    //         S: "minecraft:deepslate",
+    //         L: "minecraft:lodestone",
+    //         W: "waystones:warp_stone",
+    //         N: "minecraft:netherite_ingot",
+    //         D: "nameless_trinkets:ultimate_dust"
+    //     }
+    // )
 
     event.shaped(
         Item.of("waystones:deepslate_waystone", 1),
@@ -132,21 +132,21 @@ ServerEvents.recipes(event => {
         }
     )
 
-    event.shaped(
-        Item.of("waystones:blackstone_waystone", 1),
-        [
-            'SLS',
-            'SWS',
-            'NDN'
-        ],
-        {
-            S: "minecraft:blackstone",
-            L: "minecraft:lodestone",
-            W: "waystones:warp_stone",
-            N: "minecraft:netherite_ingot",
-            D: "nameless_trinkets:ultimate_dust"
-        }
-    )
+    // event.shaped(
+    //     Item.of("waystones:blackstone_waystone", 1),
+    //     [
+    //         'SLS',
+    //         'SWS',
+    //         'NDN'
+    //     ],
+    //     {
+    //         S: "minecraft:blackstone",
+    //         L: "minecraft:lodestone",
+    //         W: "waystones:warp_stone",
+    //         N: "minecraft:netherite_ingot",
+    //         D: "nameless_trinkets:ultimate_dust"
+    //     }
+    // )
 
     event.shaped(
         Item.of("waystones:blackstone_waystone", 1),
@@ -161,21 +161,21 @@ ServerEvents.recipes(event => {
         }
     )
 
-    event.shaped(
-        Item.of("waystones:end_stone_waystone", 1),
-        [
-            'SLS',
-            'SWS',
-            'NDN'
-        ],
-        {
-            S: "minecraft:end_stone_bricks",
-            L: "minecraft:lodestone",
-            W: "waystones:warp_stone",
-            N: "minecraft:netherite_ingot",
-            D: "nameless_trinkets:ultimate_dust"
-        }
-    )
+    // event.shaped(
+    //     Item.of("waystones:end_stone_waystone", 1),
+    //     [
+    //         'SLS',
+    //         'SWS',
+    //         'NDN'
+    //     ],
+    //     {
+    //         S: "minecraft:end_stone_bricks",
+    //         L: "minecraft:lodestone",
+    //         W: "waystones:warp_stone",
+    //         N: "minecraft:netherite_ingot",
+    //         D: "nameless_trinkets:ultimate_dust"
+    //     }
+    // )
 
     event.shaped(
         Item.of("waystones:end_stone_waystone", 1),
@@ -190,48 +190,48 @@ ServerEvents.recipes(event => {
         }
     )
 
-    event.shaped(
-        Item.of("waystones:warp_plate", 2),
-        [
-            'SDS',
-            'DHD',
-            'NWN'
-        ],
-        {
-            S: "minecraft:stone_bricks",
-            W: "waystones:warp_stone",
-            H: "waystones:dormant_shard",
-            N: "minecraft:netherite_ingot",
-            D: "waystones:warp_dust"
-        }
-    )
+    // event.shaped(
+    //     Item.of("waystones:warp_plate", 2),
+    //     [
+    //         'SDS',
+    //         'DHD',
+    //         'NWN'
+    //     ],
+    //     {
+    //         S: "minecraft:stone_bricks",
+    //         W: "waystones:warp_stone",
+    //         H: "waystones:dormant_shard",
+    //         N: "minecraft:netherite_ingot",
+    //         D: "waystones:warp_dust"
+    //     }
+    // )
 
-    event.remove([
-        {
-            id: "waystones:waystone"
-        },
-        {
-            id: "waystones:mossy_waystone"
-        },
-        {
-            id: "waystones:sandy_waystone"
-        },
-        {
-            id: "waystones:deepslate_waystone"
-        },
-        {
-            id: "waystones:blackstone_waystone"
-        },
-        {
-            id: "waystones:end_stone_waystone"
-        },
-        {
-            id: "waystones:warp_stone"
-        },
-        {
-            id: "waystones:warp_plate"
-        }
-    ])
+    // event.remove([
+    //     {
+    //         id: "waystones:waystone"
+    //     },
+    //     {
+    //         id: "waystones:mossy_waystone"
+    //     },
+    //     {
+    //         id: "waystones:sandy_waystone"
+    //     },
+    //     {
+    //         id: "waystones:deepslate_waystone"
+    //     },
+    //     {
+    //         id: "waystones:blackstone_waystone"
+    //     },
+    //     {
+    //         id: "waystones:end_stone_waystone"
+    //     },
+    //     {
+    //         id: "waystones:warp_stone"
+    //     },
+    //     {
+    //         id: "waystones:warp_plate"
+    //     }
+    // ])
 })
 
 ServerEvents.tags("item", event => {
