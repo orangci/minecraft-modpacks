1167fe5f38b190d5efab5836634739e55743117b
Stef-00012 <me@stefdp.com>
2026-07-11 18:55:59 +0200

    fix nuclear and immersive conflict

--- juniper-s11/kubejs/server_scripts/disabel_superheated_lava_bottomless_supply.js
+++
@@ -1,8 +0,0 @@
-ServerEvents.tags("fluid", event => {
-    event.remove("create:bottomless/allow", "createqol:superheated_lava")
-    event.remove("create:bottomless/allow", "createqol:flowing_superheated_lava")
-})
-
-ServerEvents.tags("item", event => {
-    event.remove("create:bottomless/allow", "createqol:superheated_lava_bucket")
-})
